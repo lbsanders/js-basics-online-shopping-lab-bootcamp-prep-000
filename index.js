e6b55888a756e86be6d65d29c28634a2626d9610
Lori Sanders <lori.sanders@me.com>
2016-11-23 15:21:25 +0000

    Done.

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function total() {
 
 function addToCart(item) {
   price = Math.floor(Math.random() * 10);
-  //cart[item] = price;
-  cart.push( {item, price} );
+  cart[item] = price;
+  //cart.push( {item, price} );
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
@@ -31,7 +31,7 @@ function getCart() {
 
 function viewCart() {
 
-  if (Object.getOwnPropertyNames(cart).length === 0) {
+  if (cart.length === 0) {
     console.log("Your shopping cart is empty.");
     return;
   } else {
